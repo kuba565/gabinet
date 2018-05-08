9747ab0d3784c3a92bac4c4559c0af64a1da2b1d
kuba565 <kuba565@gmail.com>
2018-05-08 10:32:25 +0200

    initial commit

---
+++ src/main/java/pl/coderslab/gabinet/GabinetApplication.java
@@ -0,0 +1,12 @@
+package pl.coderslab.gabinet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GabinetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GabinetApplication.class, args);
+    }
+}
